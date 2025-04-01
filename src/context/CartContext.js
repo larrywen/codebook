1efcdfea3b5974bae2a957813f89f0c9137f3102
Larry Wen <wenl@pcrb.com>
2025-04-01 15:04:40 -0400

    first commit

---
+++ src/context/CartContext.js
@@ -0,0 +1,73 @@
+import { createContext, useContext, useReducer } from "react"
+import { cartReducer } from "../reducers";
+
+const cartInitialState = {
+    cartList: [],
+    total: 0
+}
+
+
+//CartContext
+const CartContext = createContext(cartInitialState);
+
+
+//CartProvider
+export const CartProvider = ({children}) => {
+    const [state, dispatch] = useReducer(cartReducer, cartInitialState);
+
+    function addToCart(product){
+        const updatedList = state.cartList.concat(product)
+        const updatedTotal = state.total + product.price;
+
+        dispatch({
+            type: "ADD_TO_CART",
+            payload: {
+                products: updatedList,
+                total: updatedTotal
+            }
+        });
+    }
+
+    function removeFromCart(product){
+        const updatedList = state.cartList.filter(item => item.id !== product.id)
+        const updatedTotal = state.total - product.price;
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            payload: {
+                products: updatedList,
+                total: updatedTotal
+            }
+        });        
+    }
+
+    function clearCart(){
+        dispatch({
+            type: "CLEAR_CART",
+            payload: {
+                products: [],
+                total: 0
+            }
+        })
+    }
+
+
+    const value = {
+        cartList: state.cartList,
+        total : state.total,
+        addToCart,
+        removeFromCart,
+        clearCart
+    }
+
+    return (
+        <CartContext.Provider value={value} >
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+//useCart
+export  const useCart = () => {               
+    const context = useContext(CartContext);
+    return context;
+}
